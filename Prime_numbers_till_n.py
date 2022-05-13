e24fc83492569de308c6701b53fecdb3f132940e
Siddhesh Ballal <83124953+Siddhesh-Ballal@users.noreply.github.com>
2022-05-13 12:15:08 +0530

    Create Prime_numbers_till_n.py

---
+++ Prime_numbers_till_n.py
@@ -0,0 +1,15 @@
+def Prime_till(n):
+    l=[]
+    for i in range(2,n):
+        primeflag = 1;
+        for j in range(2,i):
+            if i%j==0:
+                primeflag = 0
+                break
+        if(primeflag):
+            l.append(i)
+    return l
+
+
+n = int(input("Enter number: "))
+print("Prime numbers till ",n," is: ",Prime_till(n))
